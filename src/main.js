b161756248d76ec82f5ea3311d97347952a03bed
Julian Gomez <julgomez14@gmail.com>
2023-04-25 22:25:22 -0500

    encrypt and clean functions

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
-function encriptar() {
-  const expresion = /[^a-zA-Z0-9\s]/;
-  const texto = document.getElementById('texto-a-encriptar').value;
-
-  if (expresion.test(texto)) {
+document.getElementById('encrypt').addEventListener('click', () => {
+  const text = document.getElementById('text-to-encrypt').value;
+  const expression = /[^a-zA-Z0-9\s]/;
+  if (expression.test(text)) {
     return;
   }
-
-  const newText = texto.toLowerCase().split('');
-
+  const newText = text.toLowerCase().split('');
   for (let i = 0; i < newText.length; i += 1) {
     if (newText[i] === 'e') {
       newText[i] = 'enter';
@@ -25,7 +22,33 @@ function encriptar() {
       newText[i] = 'ufat';
     }
   }
-  document.getElementById('texto-encriptado').innerHTML = newText.join('');
-}
+  document.getElementById('encrypted-text').innerHTML = newText.join('');
+});
 
-encriptar();
+document.getElementById('decrypt').addEventListener('click', () => {
+  let text = document.getElementById('text-to-encrypt').value;
+  if (text !== '') {
+    let newText = text.replace(/enter/g, 'e');
+    text = newText.replace(/imes/g, 'i');
+    newText = text.replace(/ai/g, 'a');
+    text = newText.replace(/ober/g, 'o');
+    newText = text.replace(/ufat/g, 'u');
+    document.getElementById('encrypted-text').innerHTML = newText;
+  }
+});
+
+document.getElementById('clean').addEventListener('click', () => {
+  if (document.getElementById('text-to-encrypt').value !== '') {
+    document.getElementById('text-to-encrypt').value = '';
+  }
+});
+
+document.getElementById('copy-text').addEventListener('click', () => {
+  const text = document.getElementById('encrypted-text').innerHTML;
+  if (text !== '') {
+    navigator.clipboard.writeText(text).then(() => {
+    }, () => {
+      console.error('Error al copiar texto');
+    });
+  }
+});
